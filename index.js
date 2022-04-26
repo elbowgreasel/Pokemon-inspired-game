08b61b5bb5d6de5dbef97abd4e6453f6573db68c
elbowgreasel <jadenkkropp@gmail.com>
2022-04-26 15:42:54 -0600

    added player movement animations

--- index.js
+++ index.js
@@ -49,8 +49,17 @@ image.src = './img/Lushel Coast.png'
 const foregroundImage = new Image()
 foregroundImage.src = './img/foregroundObjects.png'
 
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+const playerUpImage = new Image()
+playerUpImage.src = './img/playerUp.png'
+
+const playerLeftImage = new Image()
+playerLeftImage.src = './img/playerLeft.png'
+
+const playerDownImage = new Image()
+playerDownImage.src = './img/playerDown.png'
+
+const playerRightImage = new Image()
+playerRightImage.src = './img/playerRight.png'
 
 
 
@@ -60,9 +69,15 @@ const player = new Sprite({
         x: (canvas.width / 2) - (192 / 4) / 2,
         y: (canvas.height / 2) - (68 / 2)
     },
-    image: playerImage,
+    image: playerDownImage,
     frames: {
         max: 4  
+    },
+    sprites: {
+        up: playerUpImage,
+        left: playerLeftImage,
+        down: playerDownImage,
+        right: playerRightImage
     }
 })
 
@@ -117,7 +132,10 @@ function animate() {
     player.draw()
     foreground.draw()
     let moving = true
+    player.moving = false
     if (keys.w.pressed && lastKey === 'w'){
+        player.moving = true
+        player.image = player.sprites.up
         for (let i = 0; i < boundaries.length; i++){
             const boundary = boundaries[i]
             if (
@@ -139,6 +157,8 @@ function animate() {
         })
     }
     else if(keys.a.pressed && lastKey === 'a'){
+        player.moving = true
+        player.image = player.sprites.left
         for (let i = 0; i < boundaries.length; i++){
             const boundary = boundaries[i]
             if (
@@ -160,6 +180,8 @@ function animate() {
         })
     }
     else if(keys.s.pressed && lastKey === 's'){
+        player.moving = true
+        player.image = player.sprites.down
         for (let i = 0; i < boundaries.length; i++){
             const boundary = boundaries[i]
             if (
@@ -181,6 +203,8 @@ function animate() {
         })
     }
     else if(keys.d.pressed && lastKey === 'd'){
+        player.moving = true
+        player.image = player.sprites.right
         for (let i = 0; i < boundaries.length; i++){
             const boundary = boundaries[i]
             if (
